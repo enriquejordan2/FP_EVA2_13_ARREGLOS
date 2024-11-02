ef8e48136ef3963298438e2fd69787e3ec664107
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-11-02 10:58:43 -0600

    Add files via upload

---
+++ eva2_13_arreglos/EVA2_13_ARREGLOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_arreglos;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA2_13_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int [] --> idnica que es un arreglo de enteros 
+         //new int [10] --> crea el arreglo con 10 espacios para enteros 
+        int [] arreglo = new int [10];
+        arreglo [0] = 100;//ponemos 100 en el entero en la posicion cero
+        arreglo [9] = 900;// la ultima posicion es (10-1)
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+    }  
+}
